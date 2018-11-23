636d847eec7e2f743c60134285ec4f2351424126
TUSHAR AGRAWAL <tusharagrawal@cse.iitb.ac.in>
2018-11-23 23:02:46 +0530

    decrypts AES

--- SSL_spc/storage/views.py
+++ SSL_spc/storage/views.py
@@ -140,35 +140,38 @@ def direct_Str(request):
     if request.user.is_authenticated:
         uname = request.user.username
         usrt = models.MyUser.objects.get(username=uname)
-        iflnames = [];flnames = [];
+        iflnames = [];
+        flnames = [];
         for temp in usrt.files.all():
             iflnames.append(temp.filename)
-        strt = request.GET.get('directn', uname+'/')
+        strt = request.GET.get('directn', uname + '/')
         print(strt)
         for temp in iflnames:
             if temp.find(strt) == 0:
                 flnames.append(temp)
         print(flnames)
-        if (os.path.basename(strt)=="direct_Str"):
-            title="Files"
+        if (os.path.basename(strt) == "direct_Str"):
+            title = "Files"
         else:
-            title=strt
-        html = "<html lang=\"en\"><head><meta charset=\"ISO-8859-1\"><title>"+title+"</title><link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\" integrity=\"sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u\" crossorigin=\"anonymous\"><script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js\"></script><script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js\"></script></head><p>This is the current list of files</p> <ul> Files"
+            title = strt
+        html = "<html lang=\"en\"><head><meta charset=\"ISO-8859-1\"><title>" + title + "</title><link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\" integrity=\"sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u\" crossorigin=\"anonymous\"><script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js\"></script><script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js\"></script></head><p>This is the current list of files</p> <ul> Files"
         vfls = []
         for temp in flnames:
             tempst = temp[len(strt):]
             num = tempst.find('/')
 
-            print("tem"+temp)
+            print("tem" + temp)
             if num == -1:
-                htst = '''<li><a href="http://127.0.0.1:8000/storage/files?filename='''+urllib.parse.quote(temp,safe='')+'''">'''+tempst+"</a></li>"
-                tempht = html+htst
+                htst = '''<li><a href="http://127.0.0.1:8000/storage/files?filename=''' + urllib.parse.quote(temp,
+                                                                                                             safe='') + '''">''' + tempst + "</a></li>"
+                tempht = html + htst
                 html = tempht
             else:
                 tempst2 = tempst[:num]
                 if tempst2 not in vfls:
-                    htst = '''<li><a href="http://127.0.0.1:8000/storage/direct_Str/?directn='''+urllib.parse.quote(strt+tempst2+'/', safe='')+'''">'''+tempst2+"</a></li>"
-                    tempht = html+htst
+                    htst = '''<li><a href="http://127.0.0.1:8000/storage/direct_Str/?directn=''' + urllib.parse.quote(
+                        strt + tempst2 + '/', safe='') + '''">''' + tempst2 + "</a></li>"
+                    tempht = html + htst
                     html = tempht
                     vfls.append(tempst2)
                 else:
@@ -178,6 +181,47 @@ def direct_Str(request):
     else:
         html = "<p>You need to login to perform this operation</p>"
     return HttpResponse(html)
+    #
+    # if request.user.is_authenticated:
+    #     uname = request.user.username
+    #     usrt = models.MyUser.objects.get(username=uname)
+    #     iflnames = [];flnames = [];
+    #     for temp in usrt.files.all():
+    #         iflnames.append(temp.filename)
+    #     strt = request.GET.get('directn', uname+'/')
+    #     print(strt)
+    #     for temp in iflnames:
+    #         if temp.find(strt) == 0:
+    #             flnames.append(temp)
+    #     print(flnames)
+    #     if (os.path.basename(strt)=="direct_Str"):
+    #         title="Files"
+    #     else:
+    #         title=strt
+    #     html = "<html lang=\"en\"><head><meta charset=\"ISO-8859-1\"><title>"+title+"</title><link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css\" integrity=\"sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u\" crossorigin=\"anonymous\"><script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js\"></script><script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js\"></script></head><body><div class=\"container\"><p>This is the current list of files</p> <ul> Files"
+    #     vfls = []
+    #     for temp in flnames:
+    #         tempst = temp[len(strt):]
+    #         num = tempst.find('/')
+    #         print("tem"+temp)
+    #         if num == -1:
+    #             htst = '''<li><a href="http://127.0.0.1:8000/storage/files?filename='''+urllib.parse.quote(temp,safe='')+'''><button type=\"button\" class=\"btn btn-primary\">'''+tempst+'''</button></a></li>'''
+    #             tempht = html+htst
+    #             html = tempht
+    #         else:
+    #             tempst2 = tempst[:num]
+    #             if tempst2 not in vfls:
+    #                 htst = '''<li><a href="http://127.0.0.1:8000/storage/direct_Str/?directn='''+urllib.parse.quote(strt+tempst2+'/', safe='')+'''">'''+tempst2+"</a></li>"
+    #                 tempht = html+htst
+    #                 html = tempht
+    #                 vfls.append(tempst2)
+    #             else:
+    #                 pass
+    #         tempht = html + "</ul></div></body>"
+    #         html = tempht
+    # else:
+    #     html = "<body><div><p>You need to login to perform this operation</p></div></body>"
+    # return HttpResponse(html)
 
 def keyVerify(request):
     form = PostForm()
@@ -225,9 +269,14 @@ def display(request):
                 context["video"].append(byt);
             template=loader.get_template('web_client/video.html')
         elif (type=="text"):
-            plaindat=data[flname]
-            context["text"] = plaindat.decode('ISO-8859-1')
-            template=loader.get_template('web_client/rendertext.html')
+            plaindat = data[flname]
+            context["text"] = []
+            for byt in plaindat:
+                context["text"].append(byt);
+            template = loader.get_template('web_client/rendertext.html')
+            # plaindat=data[flname]
+            # context["text"] = plaindat.decode('ISO-8859-1')
+            # template=loader.get_template('web_client/rendertext.html')
 
         # response = HttpResponse(content_type=mimetypes.guess_type(flname))
         # response['Content-Disposition'] = 'attachment; filename=%s' % flname  # force browser to download file
--- upload_client.py
+++ upload_client.py
@@ -23,16 +23,17 @@ elif scheme=='2':
 	#print(iv)
 	aes=AES.new(key,AES.MODE_CBC,iv)
 elif scheme=='3':
-    key=sys.argv[5]
-    key=key.encode('utf-8')
-    d=des(key) 
+	key=sys.argv[5]
+	key=key.encode('utf-8')
+	d=des(key) 
 
 username = sys.argv[1]
 password = sys.argv[2]
 with open(sys.argv[3],'rb') as rf:
-    content1 = rf.read()
-    #print(len(content1))
-#print(scheme)
+	content1 = rf.read()
+
+	#print(len(content1))
+ #print(scheme)
 if scheme=='1':
 	content=arc.encrypt(content1)
 if scheme=='2':
@@ -59,11 +60,11 @@ sendcon = content.decode('ISO-8859-1')
 #print(sendcon)
 md5sum = hashlib.md5(content).hexdigest()
 with open('direct_name', 'r') as rf:
-    strtS = rf.read().strip('\n')
+	strtS = rf.read().strip('\n')
 lis2 = strtS.split('/')
 relPath = lis2[-1]+sys.argv[3][len(strtS):]
 r = rq.post('http://127.0.0.1:8000/storage/upload/', data={'name':username, 'password':password, 'filename':relPath, 'content':sendcon, 'md5':md5sum})
 rld = json.loads(r.content.decode())
 while rld['checksum'] == 'false':
-    r = rq.post('http://127.0.0.1:8000/storage/upload/', data={'name':username, 'password':password, 'filename':relPath, 'content':sendcon, 'md5':md5sum})
-    rld = json.loads(r.content.decode())
+	r = rq.post('http://127.0.0.1:8000/storage/upload/', data={'name':username, 'password':password, 'filename':relPath, 'content':sendcon, 'md5':md5sum})
+	rld = json.loads(r.content.decode())
